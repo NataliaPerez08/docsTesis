d3eaf0961c4f02d7a4d7694e8c3a9fb81c9b8c46
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-03-31 19:21:49 -0600

    Documento de tesis

--- orquestrador/Servidor/servidor.py
+++ orquestrador/Servidor/servidor.py
@@ -1,74 +1,7 @@
 import socket
-import subprocess
 from dotenv import load_dotenv
 
-def crear_interfaz_virtual(nombre_interfaz, direccion_ip, mascara_red):
-    try:
-        # Crear una interfaz virtual
-        subprocess.run(
-            ["ip", "link", "add", "dev", nombre_interfaz, "type", "wireguard"]
-        )
-
-        # Establecer la dirección IP y la máscara de red para la interfaz virtual
-        subprocess.run(
-            ["ip", "address", "add", f"{direccion_ip}/{mascara_red}", "dev", nombre_interfaz]
-        )
-
-        # Levantar la interfaz virtual
-        subprocess.run(["ip", "link", "set", "dev", nombre_interfaz, "up"])
-
-        print(
-            f"Interfaz virtual {nombre_interfaz} creada exitosamente con IP {direccion_ip}/{mascara_red}"
-        )
-    except Exception as e:
-        print(f"Error creando interfaz virtual: {e}")
-
-def iniciar_interfaz_virtual(nombre_interfaz):
-    try:
-        # Iniciar la interfaz virtual
-        subprocess.run(["wg", "set", nombre_interfaz, "up"])
-    except Exception as e:
-        print(f"Error iniciando interfaz virtual: {e}")
-
-def cargar_configuracion_wireguard(nombre_interfaz, archivo_configuracion):
-    try:
-        # Cargar la configuración de WireGuard desde el archivo de configuración
-        subprocess.run(
-            [
-                "wg",
-                "setconf",
-                nombre_interfaz,
-                archivo_configuracion,
-            ]
-        )
-    except Exception as e:
-        print(f"Error cargando la configuración de WireGuard: {e}")
-
-
-def iniciar_wireguard(listen_port, lista_peers,private_key):
-    # Crear la configuración de los peers
-    conf_peers = ""
-    for peer in lista_peers:
-        conf_peers += f"peer {peer}\n"
-        conf_peers += f"allowed-ips: {peer['allowed_ips']}\n"
-        conf_peers += f"endpoint {peer['endpoint']}\n"
-    
-    #wg set wg0 listen-port 51820 private-key /path/to/private-key peer ABCDEF... allowed-ips 192.168.88.0/24 endpoint 209.202.254.14:8172 
-    try:
-        # Iniciar WireGuard
-        subprocess.run(
-            [
-                "wg",
-                "set",
-                "wg0",
-                "listen-port",
-                listen_port,
-                private_key,
-                "/path/to/private-key",
-                conf_peers
-            ])
-    except Exception as e:
-        print(f"Error iniciando WireGuard: {e}")
+from orquestrador.WG.setup_wg import crear_interfaz_virtual, cargar_configuracion_wireguard
 
 # main
 if __name__ == "__main__":
@@ -84,8 +17,8 @@ if __name__ == "__main__":
     server_address = (direccion_ip, puerto)
     udp_socket.bind(server_address)
 
-    crear_interfaz_virtual("wg0", direccion_ip, mascara_red)
-    cargar_configuracion_wireguard("wg0", "configuracion_peers.conf")
+    #crear_interfaz_virtual("wg0", direccion_ip, mascara_red)
+    #cargar_configuracion_wireguard("wg0", "configuracion_peers.conf")
     
 
     while True:
