989dc2f96b9256b8d6597bfb881d4c05dd498112
NataliaPerez08 <natalia_perez08@comunidad.unam.mx>
2024-05-20 08:07:57 -0600

    Borrar llaver y primera version

--- OrquestradorXmlrpc/Cliente/client.py
+++ OrquestradorXmlrpc/Cliente/client.py
@@ -17,21 +17,23 @@ with xmlrpc.client.ServerProxy("http://localhost:8000/") as proxy:
         opcion = input("Opción: ")
 
         if opcion == "1":
+            print("Creando red privada...")
             input_name = input("Nombre de la red: ")
             private_network_id = proxy.create_private_network(input_name)
-            print(f"Private network ID: {private_network_id}")
+            print(f"ID de la red privada: {private_network_id}")
         elif opcion == "2":
             print("Private networks:")
             print(proxy.get_private_networks())
 
         elif opcion == "3":
-            private_network_id = input("Private network ID: ")
+            print("Ver Redes Privadas: ")
+            private_network_id = input("ID de la red privada: ")
             endpoint_ip = input("Endpoint IP: ")
             endpoint_id = proxy.create_endpoint(private_network_id, endpoint_ip)
             print(f"Endpoint ID: {endpoint_id}")
 
         elif opcion == "4":
-            private_network_id = input("Private network ID: ")
+            private_network_id = input("ID de la red privada: ")
             endpoint_ip = input("Endpoint: ")
             verificar_conectividad(endpoint_ip)
             # Si la conexion es por relay- INDIRECTA
